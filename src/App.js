7ab50fb69be5a6f3a90a1100eb62c5a4c14a8b48
Ian Emsens <ian.emsens@studiohyperdrive.be>
2022-09-07 12:48:01 +0200

    feat: add Helmet and configure og:image

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Helmet } from "react-helmet";
 
 function App() {
   return (
     <div className="App">
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>React</title>
+        <meta
+          property="og:image"
+          content="https://via.placeholder.com/640/00FF00/000000?text=This+image+was+loaded+from+react"
+        />
+      </Helmet>
+
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>React</p>
       </header>
     </div>
   );
