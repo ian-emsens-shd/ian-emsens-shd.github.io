e915381a63ffb52705862c04674783dc1fd376fe
Ian Emsens <ian.emsens@studiohyperdrive.be>
2022-09-07 15:25:57 +0200

    feat: add og:image's to page content

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@ import "./App.css";
 import { Helmet } from "react-helmet";
 
 function App() {
+  const url =
+    "https://via.placeholder.com/640/00FF00/000000?text=This+image+was+loaded+by+react";
+
   return (
     <div className="App">
       <Helmet>
         <meta charSet="utf-8" />
         <title>React</title>
-        <meta
-          property="og:image"
-          content="https://via.placeholder.com/640/00FF00/000000?text=This+image+was+loaded+from+react"
-        />
+        <meta property="og:image" content={url} />
         <meta
           name="description"
           content="This description was rendered by React."
@@ -19,6 +19,8 @@ function App() {
 
       <header className="App-header">
         <p>React</p>
+
+        <img src={url} alt="This was loaded by React"></img>
       </header>
     </div>
   );
